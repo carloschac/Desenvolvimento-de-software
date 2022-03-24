7a4c1a7fd371517e16ad19da655498431d1954ab
Carlos Henrique <carloss.chac@gmail.com>
2022-03-23 22:12:08 -0300

    Update parImpar.java

--- Aula3/parImpar.java
+++ Aula3/parImpar.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class parImpar {
     public static String verificarparImpar(int n) {
         if (n % 2 == 0) {
@@ -6,13 +8,22 @@ public class parImpar {
             return "Impar";
         }
     }
-}
-public static void main(String[] args) {
-    Scanner teclado = new.Scanner(System.in);
-    int n;
-    do {
-        int n = teclado.nextInt();
-        if (n >= 0)
-            System.out.println(verificarparImpar(n));
+
+    //função principal
+
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int n;
+
+        do {
+            n = teclado.nextInt();
+            if (n >= 0) {
+                System.out.println(verificarparImpar(n));
+            }
+        } while (n >= 0);
+        teclado.close();
     }
 }
+
