37e92ebbac02231e0e9411f8fc58976e6e8399c4
Carlos Henrique <carloss.chac@gmail.com>
2022-05-11 21:30:18 -0300

    commit

---
+++ aula10/grafica/FrmCalculadoraGeometrica.java
@@ -0,0 +1,12 @@
+package aula10.grafica;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class FrmCalculadoraGeometrica extends JFrame{
+    public FrmCalculadoraGeometrica() {
+        setSize(new Dimension(720, 600));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
--- aula10/logica/ProgramaPrincipal.java
+++ aula10/logica/ProgramaPrincipal.java
@@ -1,18 +1,13 @@
 package aula10.logica;
 
+import aula10.grafica.FrmCalculadoraGeometrica;
+
 public class ProgramaPrincipal {
 
 
     public static void main(String[] args) {
-        FormaGeometrica vetor[] = new FormaGeometrica[3];
-            
-        vetor[0] = new Circulo(0.5);
-        vetor[1] = new Retangulo(2.5, 3);
-        vetor[2] = new Triangulo(3, 6);
-
-        for (FormaGeometrica f: vetor) {
-        System.out.printf("%s -> area: %f -> cor: %s\n", 
-            f.getClass().getName(), f.calcularArea(), f.preencherCor());
-        }
+       FrmCalculadoraGeometrica frm = new FrmCalculadoraGeometrica();
+       frm.setVisible(true);
+       
     }
 }
